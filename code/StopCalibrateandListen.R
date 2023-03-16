0ecee19d4293ee4fa15f253220d7535a252c685b
Gabriel Bowen <gabe.bowen@utah.edu>
2023-03-15 18:42:13 -0600

    Update recalibration

--- code/StopCalibrateandListen.R
+++ code/StopCalibrateandListen.R
@@ -5,18 +5,22 @@ ForensicTIsoData <- read_csv("data/ForensicIsoData.csv",
                              col_types = cols(...1 = col_skip()))
 ##Oye, not coperating on my computer
 
-testhair <- subset(ForensicTIsoData, Element == 'hair' & Calibrate == 'OldUT_O_1') %>% 
+testhair <- subset(ForensicTIsoData, Element == 'hair' & Isotope == 'd18O') %>% 
   rename(d18O  = Iso.Value,
         d18O_cal = Calibrate)
 
 #add in SD
 testhair$d18O.sd <-0.3
 
+#reftrans can't handle data with no calibration scale
+toTrans = data.frame(testhair[!is.na(testhair$d18O_cal),])
+
 #trying the refTrans, needs calibration scale field for the selected marker
-e = refTrans(testhair, marker = "d18O", ref_scale = "VSMOW_O", niter = 5000)
+e = refTrans(toTrans, marker = "d18O", ref_scale = "VSMOW_O")
 
-#doesn't like this one, says marker.sd unused
-e = refTrans(testhair, marker = "d18O", ref_scale = "VSMOW_O", marker.sd = "d18O.sd")
+#let's compare pre and post trans; sample order is different so we need
+#to match IDs to plot pre and post from the same samples
+ind = match(toTrans$Data.ID, e$data$Data.ID)
 
-
-
+#plot d18O
+plot(toTrans$d18O, e$data$d18O[ind])
