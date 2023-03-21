ae7ab365d9ec8390ab9f533a38af38e8bf236be5
Kverostick <101838721+Kverostick@users.noreply.github.com>
2023-03-21 13:10:04 -0600

    Update StopCalibrateandListen.R

--- code/StopCalibrateandListen.R
+++ code/StopCalibrateandListen.R
@@ -1,4 +1,5 @@
-library(terra);library(sf); library(readr); library(assignR); library(dplyr)
+library(terra);library(sf); library(readr); library(assignR); library(dplyr); library(assignR)
+library(raster)
 #not sure which packages are actually being used here...
 
 ForensicTIsoData <- read_csv("data/ForensicIsoData.csv", 
@@ -24,3 +25,44 @@ ind = match(toTrans$Data.ID, e$data$Data.ID)
 
 #plot d18O
 plot(toTrans$d18O, e$data$d18O[ind])
+
+#Oxygen isoscape of calibrated data
+prpiso = getIsoscapes("GlobalPrecipMA")
+prpiso = stack(prpiso$d18o_MA, prpiso$d18o_se_MA)
+
+hspdf =SpatialPointsDataFrame(data.frame(e$data$Lon, e$data$Lat),
+                              data.frame(e$data$d18O, e$data$d18O.sd))
+
+proj4string(hspdf) <-CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+hairscape= calRaster(hspdf, prpiso, mask = naMap)
+
+#now isoscape uncalibrated oxygen hairs
+regularhair <- subset(ForensicTIsoData, Element == 'hair' & Isotope == 'd18O') %>% 
+  rename(d18O  = Iso.Value,
+         d18O_cal = Calibrate)
+
+regularhair$d18O.sd <-0.3
+
+#rhspdf =SpatialPointsDataFrame(data.frame(regularhair$data$Lon, regularhair$data$Lat),
+#                              data.frame(regularhair$data$d18O, regularhair$data$d18O.sd))
+
+rhspdf =SpatialPointsDataFrame(data.frame(regularhair$Lon, regularhair$Lat),
+                               data.frame(regularhair$d18O, regularhair$d18O.sd))
+
+proj4string(rhspdf) <-CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+reghairscape= calRaster(rhspdf, prpiso, mask = naMap)
+
+#teeth time, teeth oxygen isoscape
+teeth <- subset(ForensicTIsoData, Element == 'teeth' & Isotope == 'd18O') %>% 
+  rename(d18O  = Iso.Value,
+         d18O_cal = Calibrate)
+teeth$d18O.sd <-0.3
+
+tspdf =SpatialPointsDataFrame(data.frame(teeth$Lon, teeth$Lat),
+                              data.frame(teeth$d18O, teeth$d18O.sd))
+
+proj4string(tspdf) <-CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+teethscape= calRaster(tspdf, prpiso, mask = naMap)
